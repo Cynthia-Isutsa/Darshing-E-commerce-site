89f6d001530e18ebebd5392f27fd83f2d8468477
Cynthia-Isutsa <isutsacynthia@gmail.com>
2023-09-16 16:08:13 +0300

    Home

--- Darshing-Commerce/src/Pages/Home.jsx
+++ Darshing-Commerce/src/Pages/Home.jsx
@@ -1,17 +1,34 @@
 import React, { useContext } from 'react'
 //import product context
 import { ProductContext } from '../contexts/ProductContext'
+import { Product } from '../components'
 
 
 const Home = () => {
 
   const {products} = useContext(ProductContext)
-  console.log(products)
-  
+
+   
+//get onl men's and women's clothing 
+const filteredProducts = products.filter((item) =>{
   return (
-    <div>
-      Home Page
-    </div>
+    item.category === "men's clothing" || item.category === "women's clothing"
+    )
+})
+console.log(filteredProducts)
+  return (
+    <section className='py-16'>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+          {filteredProducts.map((product) =>{
+            return(
+              <Product />
+            )
+          })}
+
+        </div>
+      </div>
+    </section>
   )
 }
 
