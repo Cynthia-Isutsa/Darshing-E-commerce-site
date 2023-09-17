3c5dfc051ea69e3c82c78a56ffc8e886f3c6274e
Cynthia-Isutsa <isutsacynthia@gmail.com>
2023-09-17 22:02:05 +0300

    button

--- Darshing-Commerce/src/components/Product.jsx
+++ Darshing-Commerce/src/components/Product.jsx
@@ -17,8 +17,13 @@ const { id, image, title, category, price} = product
           </div>
         </div>
         {/*buttons */}
-        <div className='absolute top-0 right-0'>
-          <button>Add to Cart</button>
+        <div className='absolute top-0 right-0 bg-red-500'>
+          <button>
+            <div className='flex justify-center items-center w-12 text-white h-12'>
+              <BsPlus className='text-3xl'/>
+            </div>
+          </button>
+          <Link to></Link>
         </div>
       </div>
       <div>2</div>
